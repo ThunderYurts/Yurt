24ac70cb94267cdf85a9451270d57af2908694ed
ty0207 <ty0207@sjtu.edu.cn>
2020-06-15 23:27:45 +0800

    modify entry and prepare dockerfile

---
+++ docker-entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -eo pipefail
+shopt -s nullglob
+
+yurt_name="yurt"
+if [ "$YURT_NAME" ] ;then
+  yurt_name=$YURT_NAME
+fi
+
+action_port=":40000"
+if [ "$ACTION_PORT" ] ;then
+  action_port=$ACTION_PORT
+fi
+
+ip="127.0.0.1"
+if [ "$HOST_IP" ] ;then
+  ip=$HOST_IP
+fi
+
+log_path="./yurt.log"
+if [ "$LOG_PATH" ] ;then
+  log_path=$LOG_PATH
+fi
+
+sync_port=":39999"
+if [ "$SYNC_PORT" ] ;then
+  sync_port=$SYNC_PORT
+fi
+
+zookeeper_addr="106.15.225.249:3030"
+if [ "$ZOOKEEPER_ADDR" ] ;then
+  zookeeper_addr=$ZOOKEEPER_ADDR
+fi
+
+/root/app -n "$yurt_name" -ap "$action_port" -ip "$ip" -l "$log_path" -sp "$sync_port" -zk "$zookeeper_addr"
--- main.go
+++ main.go
@@ -6,22 +6,29 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/ThunderYurts/Yurt/yurt"
 )
 
 var (
 	help       bool
+	name       string
 	syncPort   string
 	actionPort string
 	logName    string
+	ip         string
+	zkAddr     string
 )
 
 func init() {
 	flag.BoolVar(&help, "h", false, "this help")
-	flag.StringVar(&syncPort, "sp", ":50000", "set syncPort port")
-	flag.StringVar(&actionPort, "ap", ":50001", "set actionPort port")
+	flag.StringVar(&name, "n", "yurt", "set yurt name")
+	flag.StringVar(&ip, "ip", "127.0.0.1", "set node ip")
+	flag.StringVar(&syncPort, "sp", ":39999", "set syncPort port")
+	flag.StringVar(&actionPort, "ap", ":40000", "set actionPort port")
 	flag.StringVar(&logName, "l", "./yurt.log", "set log name")
+	flag.StringVar(&zkAddr, "zk", "106.15.225.249:3030", "set zeus connection zookeeper cluster")
 }
 
 func main() {
@@ -32,11 +39,12 @@ func main() {
 	}
 	rootContext, finalizeFunc := context.WithCancel(context.Background())
 
-	yurt, err := yurt.NewYurt(rootContext, finalizeFunc, "primary", logName)
+	yurt, err := yurt.NewYurt(rootContext, finalizeFunc, name, logName)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	err = yurt.Start(syncPort, actionPort)
+	addrs := strings.Split(zkAddr, ";")
+	err = yurt.Start(ip, syncPort, actionPort, addrs)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
